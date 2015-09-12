d515b21304018300e430016bd8e4c4d947455ab8
RedPaes <meyer.pascal92@gmail.com>
2015-09-12 10:41:36 +0200

    init

---
+++ Bank.java
@@ -0,0 +1,35 @@
+package m326;
+
+public class Bank {
+
+	private Integer bankleitZahl;
+	private String name;
+	
+	public void kontenBerichtErstellen() {
+		
+	}
+	public void kontoueberpruefen() {
+		
+	}
+	
+	public void transaktionVerarbeiten() {
+		
+	}
+	
+	public Boolean ueberpruefeGuthaben(Float betrag) {
+		return null;
+	}
+	
+	public Integer getBankleitZahl() {
+		return bankleitZahl;
+	}
+	public void setBankleitZahl(Integer bankleitZahl) {
+		this.bankleitZahl = bankleitZahl;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ Verwaltungszentrale.java
@@ -0,0 +1,35 @@
+package m326;
+
+public class Verwaltungszentrale {
+
+	private Integer zentralenNummer;
+	private Integer anzahlATMs;
+	
+	public void transaktionAbwickeln() {
+		
+	}
+	
+	public void kostenUmlegen() {
+		
+	}
+	
+	public Integer anzahlATMs() {
+		return null;
+	}
+
+	public Integer getZentralenNummer() {
+		return zentralenNummer;
+	}
+
+	public void setZentralenNummer(Integer zentralenNummer) {
+		this.zentralenNummer = zentralenNummer;
+	}
+
+	public Integer getAnzahlATMs() {
+		return anzahlATMs;
+	}
+
+	public void setAnzahlATMs(Integer anzahlATMs) {
+		this.anzahlATMs = anzahlATMs;
+	}
+}
